d1420574e69cbb671170948baf25608ab2bae1a5
Aseel Al Dallal <aseelaldallal@gmail.com>
2016-11-23 05:14:48 +0000

    Make event.ejs list all events in db

--- app.js
+++ app.js
@@ -3,12 +3,42 @@
 /***************************************************/
 
 var express             = require("express"),
-    bodyParser          = require("body-parser");
-
+    bodyParser          = require("body-parser"),
+    mongoose            = require("mongoose");
+    
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
     
+mongoose.connect("mongodb://localhost/event_app");
+
+// SCHEMA SETUP
+var eventSchema = new mongoose.Schema({
+   name: String,
+   date: Date,
+   location: String,
+   image: String,
+   description: String
+});
+
+var Event = mongoose.model("Event", eventSchema);
+
+/*Event.create( 
+    {   name: "Mo's Birthday",
+        date: new Date(2017,05,11),
+        location: "Pokemon World",
+        image: "https://farm4.staticflickr.com/3692/12414882233_cfa96bb2b2.jpg",
+        description: "Great bday"
+    }, function(err, createdEvent) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(createdEvent)
+        }
+    }
+);
+*/
 
 /***************************************************/
 /********************* ROUTES **********************/
@@ -22,7 +52,13 @@ app.get("/", function(req,res) {
 
 // INDEX ROUTE - DISPLAY A LIST OF ALL EVENTS
 app.get("/events", function(req,res) {
-    res.render("events");
+    Event.find({}, function(err, foundEvents) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("events", {events: foundEvents}); // Change to index later 
+        }
+    });
 });
 
 // NEW ROUTE - DISPLAY FORM TO CREATE A NEW EVENT
